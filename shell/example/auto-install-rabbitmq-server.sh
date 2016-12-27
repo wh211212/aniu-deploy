54689635858bf3f9ddd84c285a6d4cfc86e466db
shaonbean <wh211212@qq.com>
2016-12-27 10:00:01 +0800

    initial

--- shell/example/auto-install-rabbitmq-server.sh
+++ shell/example/auto-install-rabbitmq-server.sh
@@ -10,16 +10,40 @@
 # Functions: The script be used for auto install RabbitMQ  #
 # ENV: CentOS release 6.8 (Final) 
 
+################################################
+#               configure repo                 #
+################################################
+epel-repo() {
+#rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+# rpm -ivh http://mirrors.aliyun.com/epel/epel-release-latest-6.noarch.rpm
+# cd /usr/local/src
+# wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+# unstall aliyun epel
+rpm -qa | grep epel
+rpm -e epel-release-6-8.noarch 
+find / -type d -name epel -exec rm -rf {} \;
+# install fedoraproject epel
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+if [ $? -eq 0 ];then
+  echo " intsall epel repo succeed!"
+  else
+  echo " install epel repo failed!" 
+}
+
 ##################################################################
 #    Configure system hosts, scripts use rabbitmq-1,2 example    #
 ##################################################################
 #
 config-host() {
 ip=`ifconfig | grep -v '127.0.0.1' | awk '/inet addr/{print substr($2,6)}'`
-host=(rabbitmq-1)
-echo "${ip} $host" >> /etc/hosts
+host=`hostname`
+egrep "${ip} ${host}" /etc/hosts >/dev/null 2>&1
+if [ $? -eq 0 ];then
+  echo " ******　Host has been configured! ******"
+  else
+  echo "${ip} ${host}" >> /etc/hosts
+fi
 }
-
 
 #######################################################################
 #    Remove Erlang & RabbitMQ if exists                               #
@@ -29,19 +53,26 @@ remove-rpm() {
 # remove erlang
 rpm -qa | grep erlang 
 if [ $? -eq 0 ];then
-  echo "Erlang has been installed. Need to uninstall first!"
+  echo "*** Erlang has been installed. Need to uninstall first! ***"
   yum remove erlang* -y
+# remove associate dir  
+#  for package in erlang rabbitmq-server
+#  do
+  find / -type d -name erlang -exec rm -rf {} \;
+#  done
   else
-  echo "Erlang has not been installed. Don't need to uninstall!"
+  echo "*** Erlang has not been installed. Don't need to uninstall! ***"
 fi
 
 # uninstall rabbitmq
  rpm -qa | grep rabbitmq-server
  if [ $? -eq 0 ];then
-   echo "Erlang has been installed. Need to uninstall first!"
+   echo "*** RabbbitMQ has been installed. Need to uninstall first! ***"
    yum remove rabbitmq-server -y
+   find / -type d -name erlang -exec rm -rf {} \;
    else              
-   echo "Erlang has not been installed. Don't need to uninstall!"
+   echo "*** RabbitMQ has not been installed. Don't need to uninstall! ***"
+   echo ""
  fi
 }
 
@@ -50,19 +81,27 @@ fi
 ########################################################################
 create-repo(){
 # create erlang solutions repo
-cat > /etc/yum.repos.d/erlang-solutions.repo << EOF
+cat > /etc/yum.repos.d/erlang-solutions.repo << 'EOF'
 [erlang-solutions]
 name=Centos $releasever - $basearch - Erlang Solutions
 baseurl=https://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch
 gpgcheck=1
 gpgkey=https://packages.erlang-solutions.com/rpm/erlang_solutions.asc
 enabled=1
-EOF 
+EOF
+# to determine whether a source to create succeed
+if [ $? -eq 0 ];then
+    echo "*** create erlang solutions repo succeed! ***"
+    echo ""
+  else
+    echo "*** create erlang solutions repo failed! ***" 
+    exit 0
+fi
 
 # create rabbitmq-server.repo 
-cat > /etc/yum.repos.d/rabbitmq-server.repo << EOF
-[erlang-solutions]
-name=rabbitmq_rabbitmq-server
+cat > /etc/yum.repos.d/rabbitmq-server.repo << 'EOF'
+[rabbitmq-server]
+name=rabbitmq-server
 baseurl=https://packagecloud.io/rabbitmq/rabbitmq-server/el/6/$basearch
 repo_gpgcheck=1
 gpgcheck=0
@@ -83,6 +122,14 @@ sslverify=1
 sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 metadata_expire=300
 EOF
+#
+if [ $? -eq 0 ];then
+    echo "*** create rabbitmq repo succeed! ***"
+    echo ""
+  else
+    echo "*** create rabbitmq repo failed! ***" 
+    exit 0
+fi
 }
 
 #########################################################
@@ -94,7 +141,7 @@ yum clean all && yum makecache
 # yum install erlang & rabbitmq-server
 if [ $? -eq 0 ];then
   echo " Erlang repo & rabbitmq-server repo have been created succeed! "
-# yum install erlang -y && yum install rebbitmq-server -y
+  yum install erlang -y && yum install rabbitmq-server -y
   else
   echo " Erlang repo & rabbitmq-server repo have been created failed! "
   exit 0
@@ -158,13 +205,17 @@ rabbitmq-plugins enable rabbitmq_management
 #   Define main function call child functions          #
 ########################################################
 main() {
+epel-repo
 config-host
-#remove-rpm
-#create-repo
-#yum-install
+remove-rpm
+create-repo
+yum-install
 #use-rabbitmq
 #rabbitmq-manage
 }
+
+# Suggestion:
+# If you know erlang & rabbitmq don't installed, you can nonuse remove-rpm functions, if you use remove-rpm,after execute scripts succeed remove remove-rpm from the main functions.
 
 main
 
