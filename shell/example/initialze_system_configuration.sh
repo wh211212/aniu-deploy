28ca43f5dfd6d556a30c96b6f925076c1b169437
shaonbean <wh211212@qq.com>
2016-12-30 10:46:01 +0800

    initial

---
+++ shell/example/initialze_system_configuration.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+##########################################################################
+# Script Name: initialze_system_configuration.sh
+# Author: shaon
+# Email: shaonbean@qq.com
+# Created Time: Fri 30 Dec 2016 10:30:28 AM CST
+#########################################################################
+# Blog address: http://blog.csdn.net/wh211212
+#########################################################################
+# Functions: initialze system configuration #
+# ENV: CentOS 7
+# Define some variables:  #
+# add user
+#########################################################################
+add-user() {
+  #
+  user=devops
+  egrep ${user} /etc/passwd >/devnull 2>&1
+  if [ $? -eq 0 ];then
+    echo "*** ${user} already exists! ***"
+    else
+    useradd ${user}
+    echo anwg123. | passwd --stdin ${user}
+    mkdir -p /home/${user}/{backup,scripts}
+    echo "*** user add succeed!  ***"
+  fi
+}
+
+#
+
