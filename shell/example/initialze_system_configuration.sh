84d425f7baba84e850427941b7ad8e04301e9a88
shaonbean <wh211212@qq.com>
2016-12-31 10:00:01 +0800

    initial

--- shell/example/initialze_system_configuration.sh
+++ shell/example/initialze_system_configuration.sh
@@ -10,7 +10,17 @@
 # Functions: initialze system configuration #
 # ENV: CentOS 7
 # Define some variables:  #
-# add user
+#########################################################################
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root execute script!"
+    exit 0
+fi
+# 
+echo "+------------------------------------------------------------------------+"
+echo "|       To initialization the system for security and performance        |"
+echo "+------------------------------------------------------------------------+"
+
 #########################################################################
 add-user() {
   #
@@ -25,8 +35,21 @@ add-user() {
     echo "*** user add succeed!  ***"
     # grant user
     echo "${user}  ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
+    # Make a user be only a user who can switch to root as an administration user
+    #usermod -G wheel devops
+    #echo "auth            required        pam_wheel.so use_uid" >> /etc/pam.d/su
   fi
 }
 
-#
+############################################################################
+disable-firewall() {
+  #
+  echo "*** disable firewall & selinux! ***"
+  systemctl stop firewalld && systemctl disable firewalld 
+  #
+  sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+  #
+  setenforce 0
+}
 
+#
